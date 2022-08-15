d0d4381fe740ceebdb0f433114e562ee74d4bc91
ialej001 <ivan.alejandre@gmail.com>
2022-08-14 20:47:45 -0700

    set up mock API server for carbon estimates
    add in two new env url configs

---
+++ src/components/dummyComponent.tsx
@@ -0,0 +1,11 @@
+// JUST TO CREATE THE FOLDER. THIS IS A GARBAGE FILLER COMPONENT
+
+import React from 'react'
+
+const dummyComponent = () => {
+  return (
+    <div>dummyComponent</div>
+  )
+}
+
+export default dummyComponent
