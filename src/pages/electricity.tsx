fd8e60f416381f775eabbfbdfe253a699d34eb6d
Victoria Cheng <victoria.cheng15@gmail.com>
2022-08-16 14:49:44 -0400

    add skeleton content for the electricity consupmtion page

---
+++ src/pages/electricity.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+export default function ElectricityConsumption() {
+  return (
+    <div>
+      <Head>
+        <title>Electricity Consumption</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <nav className="flex justify-center gap-4">
+        <Link href='/'>Home</Link>
+      </nav>
+      <h2>Electricity Consumption Page</h2>
+    </div>
+  )
+}
