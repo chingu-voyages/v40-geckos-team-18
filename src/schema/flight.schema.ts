5ab39736839245eca5980c17205a03507db8dd50
ialej001 <ivan.alejandre@gmail.com>
2022-08-18 18:24:10 -0700

    refactor flightLeg schema for potential validation

--- src/schema/flight.schema.ts
+++ src/schema/flight.schema.ts
@@ -1,32 +1,33 @@
 import z from 'zod';
 
-const cabinClassSchema = z.enum(['economy', 'premium'])
+const cabinClassSchema = z.enum(['economy', 'premium']);
+
+const flightLegAirportSchema = z
+  .string()
+  .length(3, 'Must enter IATA code (airport code)');
+
 const flightLegSchema = z.object({
-  departure_airport: z
-    .string()
-    .length(3, 'Must enter IATA code (airport code)'),
-  destination_airport: z
-    .string()
-    .length(3, 'Must enter IATA code (airport code)'),
+  departure_airport: flightLegAirportSchema,
+  destination_airport: flightLegAirportSchema,
   cabin_class: cabinClassSchema,
 });
 
-const distanceUnitSchema = z.enum(['km', 'mi'])
+const distanceUnitSchema = z.enum(['km', 'mi']);
 
 export const unregisteredFlightRequestSchema = z.object({
   passengers: z.number(),
   distance_unit: distanceUnitSchema,
-  legs: z.array(
-    flightLegSchema
-  ),
+  legs: z.array(flightLegSchema),
 });
 
-export type CabinClass = z.TypeOf<typeof cabinClassSchema>
-export type FlightLeg = z.TypeOf<typeof flightLegSchema>
+export type CabinClass = z.TypeOf<typeof cabinClassSchema>;
+export type FlightLeg = z.TypeOf<typeof flightLegSchema>;
 
-export type DistanceUnit = z.TypeOf<typeof distanceUnitSchema>
+export type DistanceUnit = z.TypeOf<typeof distanceUnitSchema>;
 
-export type UnregisteredFlightRequest = z.TypeOf<typeof unregisteredFlightRequestSchema>
+export type UnregisteredFlightRequest = z.TypeOf<
+  typeof unregisteredFlightRequestSchema
+>;
 
 const flightResponseUnqiue = z.object({
   carbon_g: z.number(),
@@ -34,8 +35,9 @@ const flightResponseUnqiue = z.object({
   carbon_lb: z.number(),
   carbon_mt: z.number(),
   estimated_at: z.string(),
-})
+});
 
-const flightResponseSchema = unregisteredFlightRequestSchema.merge(flightResponseUnqiue)
+const flightResponseSchema =
+  unregisteredFlightRequestSchema.merge(flightResponseUnqiue);
 
-export type FlightResponse = z.TypeOf<typeof flightResponseSchema>
+export type FlightResponse = z.TypeOf<typeof flightResponseSchema>;
