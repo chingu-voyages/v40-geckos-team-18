02dfe65ded1ae06e48b832c360edb1ced56f48a5
ialej001 <ivan.alejandre@gmail.com>
2022-08-18 21:23:32 -0700

    added image and styling

--- src/pages/travel.tsx
+++ src/pages/travel.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link';
 import Head from 'next/head';
-import { Tabs } from 'flowbite-react';
+import { Accordion, Tabs } from 'flowbite-react';
 import { FaPlane, FaCarAlt } from 'react-icons/fa';
 import FlightQuestionaire from '../components/Flight/FlightQuestionaire';
+import wateringPlantsImage from '../assets/images/watering-plants-removebg-preview 1.png';
+import Image from 'next/image';
 
 export default function TravelPage() {
   return (
@@ -15,16 +17,42 @@ export default function TravelPage() {
         <Link href="/">Home</Link>
       </nav>
 
-      <div className="flex justify-center">
-        <Tabs.Group aria-label="Pills" style="pills">
+      <div className='flex justify-center'>
+        <Image src={wateringPlantsImage} />
+      </div>
+
+      <div className="mx-auto flex justify-center w-1/3">
+        <Tabs.Group aria-label="Tabs with underline" style="underline">
           <Tabs.Item active={true} title="Flying" icon={FaPlane}>
             <FlightQuestionaire />
           </Tabs.Item>
           <Tabs.Item title="Driving" icon={FaCarAlt}>
             Your Driving parent component here
+            <FlightQuestionaire />
           </Tabs.Item>
         </Tabs.Group>
       </div>
+
+      {/* <div className='flex mx-auto min-w-fit w-96 justify-center'>
+        <Accordion alwaysOpen={true}>
+          <Accordion.Panel>
+            <Accordion.Title>
+              Flying
+            </Accordion.Title>
+            <Accordion.Content>
+              <FlightQuestionaire />
+            </Accordion.Content>
+          </Accordion.Panel>
+          <Accordion.Panel>
+            <Accordion.Title>
+              Driving
+            </Accordion.Title>
+            <Accordion.Content>
+              Driving stufff
+            </Accordion.Content>
+          </Accordion.Panel>
+        </Accordion>
+      </div> */}
     </div>
   );
 }
