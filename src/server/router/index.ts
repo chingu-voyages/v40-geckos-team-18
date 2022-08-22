42a9596dc3b5b51ae4459b14ff6ba22daa120e82
ialej001 <ivan.alejandre@gmail.com>
2022-08-22 12:03:15 -0700

    added flight router after merge conflict

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -5,12 +5,14 @@ import superjson from "superjson";
 import { exampleRouter } from './example';
 import { protectedExampleRouter } from './protected-example-router';
 import { electricityRouter } from './electricity.router';
+import { flightRouter } from "./flight.router";
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge('example.', exampleRouter)
   .merge('question.', protectedExampleRouter)
-  .merge('electricity.', electricityRouter);
+  .merge('electricity.', electricityRouter)
+  .merge('flight.', flightRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
