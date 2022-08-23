7349605960fe09fcc2b2e3806856972d5f9dba88
Ticia <84279128+TiciaD@users.noreply.github.com>
2022-08-23 17:16:28 -0400

    Feature/td ticket20 fuel backend (#64)
    * add files for schema and router
    
    * Feature - Setup Fuel backend
      - setup schema for fuel request/response
      - setup router for fuel endpoint
      - added fuel endpoint to tRPC router

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -5,12 +5,14 @@ import superjson from 'superjson';
 import { exampleRouter } from './example';
 import { protectedExampleRouter } from './protected-example-router';
 import { electricityRouter } from './electricity.router';
+import { fuelRouter } from './fuel.router';
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge('example.', exampleRouter)
   .merge('question.', protectedExampleRouter)
-  .merge('electricity.', electricityRouter);
+  .merge('electricity.', electricityRouter)
+  .merge('fuel.', fuelRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
