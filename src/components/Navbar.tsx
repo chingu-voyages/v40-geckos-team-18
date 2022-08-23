ba5f1ad8593cbd87138ad222e71f73ff46b163c6
Cian Butler <67752829+ciandevelops@users.noreply.github.com>
2022-08-22 23:58:43 -0400

    basic navbar layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,27 @@
+import Link from 'next/link';
 import React from 'react';
-
+import { HiMenuAlt3 } from 'react-icons/hi';
 const Navbar = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <nav className="flex justify-between p-4">
+      <div className="">
+        <Link href="/">CarbonCalc</Link>
+      </div>
+
+      <div>
+        <div>
+          <HiMenuAlt3 className="sm:hidden" size={20} />
+        </div>
+        <div className="hidden sm:flex">
+          <p className="px-4">
+            <Link href="/">Start Here</Link>
+          </p>
+          <p className="px-4">
+            <Link href="/login">Login</Link>
+          </p>
+        </div>
+      </div>
+    </nav>
   );
 };
 
