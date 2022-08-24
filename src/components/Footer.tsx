1e9c762ef00f06c108d0b55b3374306d89cd2783
Amanda Vieira <amanda.alexandre@outlook.com>
2022-08-24 09:58:40 -0300

    feat: adding placeholders as footer and navbar

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+// JUST TO CREATE THE FOLDER. THIS IS A GARBAGE FILLER COMPONENT
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+// JUST TO CREATE THE FOLDER. THIS IS A GARBAGE FILLER COMPONENT
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar;
