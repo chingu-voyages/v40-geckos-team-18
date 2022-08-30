6fcc6e25498fcc91e759f1afaa0aadbd7553ffdc
Cian Butler <67752829+ciandevelops@users.noreply.github.com>
2022-08-30 12:25:10 -0400

    Component/navbar (#66)
    * navbar comp created and imported
    
    * hello world!
    
    * basic navbar layout
    
    * mobile nav working
    
    * removed flowbite component version
    
    * bold font added
    
    * typo
    
    * removed unused flowbite
    
    * nav links updated on desktop

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 // src/pages/_app.tsx
-import { withTRPC } from "@trpc/next";
-import type { AppRouter } from "../server/router";
-import type { AppType } from "next/dist/shared/lib/utils";
-import superjson from "superjson";
-import { SessionProvider } from "next-auth/react";
-import "../styles/globals.css";
+import { withTRPC } from '@trpc/next';
+import type { AppRouter } from '../server/router';
+import type { AppType } from 'next/dist/shared/lib/utils';
+import superjson from 'superjson';
+import { SessionProvider } from 'next-auth/react';
+import '../styles/globals.css';
+import Nav from '../components/Nav';
 
 const MyApp: AppType = ({
   Component,
@@ -12,14 +13,15 @@ const MyApp: AppType = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Nav />
       <Component {...pageProps} />
     </SessionProvider>
   );
 };
 
 const getBaseUrl = () => {
-  if (typeof window !== "undefined") {
-    return "";
+  if (typeof window !== 'undefined') {
+    return '';
   }
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
