c2d933585687207785c65027f53fc8b3d4ab90c1
Cian Butler <67752829+ciandevelops@users.noreply.github.com>
2022-08-31 13:28:38 -0400

    basic styling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Card = () => {
-  return <div className="p-4">{/* CARD */}</div>;
+  return (
+    <div className="p-4">
+      {/* CARD */}
+
+      <div className="card h-[335px] w-[280px] flex flex-col items-center justify-center bg-secondary">
+        <img src="https://via.placeholder.com/100" alt="/" />
+        <p className="text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. A praesentium
+          sint quasi officiis vitae
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
