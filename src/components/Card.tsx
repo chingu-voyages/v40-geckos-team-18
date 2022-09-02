b53c24b4c365327be33c8417f1fe9c127b857fc3
Cian Butler <67752829+ciandevelops@users.noreply.github.com>
2022-09-02 18:02:43 -0400

    props moved into correct files

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
-const Card = () => {
+
+interface CardProps {
+  image: StaticImageData;
+  description: string;
+  link: string;
+}
+
+const Card = ({ image, description, link }: CardProps) => {
   return (
-    <div className="bg-secondary h-[335px] w-[280px] flex flex-col items-center justify-center space-y-4 card">
-      <img
-        src="https://via.placeholder.com/150"
-        alt="placeholder"
-        height={70}
-        width={65}
-      />
-      <p className="text-center font-bold text-white">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ut
-        alias explicabo exercitationem quo impedit minus error recusandae natus
-        ab neque et, at quas fugit!
-      </p>
-    </div>
+    <Link href={link}>
+      <div className="bg-secondary h-[335px] w-[280px] flex flex-col items-center justify-center space-y-4 card cursor-pointer">
+        <Image src={image} alt="placeholder" height={70} width={65} />
+        <p className="text-center font-bold text-white">{description}</p>
+      </div>
+    </Link>
   );
 };
 
--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -5,37 +5,37 @@ import plane from '../assets/images/plane.png';
 import house from '../assets/images/house.png';
 import shipping from '../assets/images/shipping.png';
 
-const Cards = ({}) => {
+const Cards = () => {
   const cardInfo = [
     {
-      id: 0,
       image: plane,
       description: 'How much your flights cost to the environment?',
       link: '/travel',
     },
     {
-      id: 1,
       image: car,
       description: 'How much your car cost to the environment?',
       link: '/fuel',
     },
     {
-      id: 2,
       image: house,
       description: 'How much your house cost to the environment?',
       link: '/electricity',
     },
     {
-      id: 3,
       image: shipping,
       description: 'How much your online shopping cost to the environment?',
-      link: '/',
+      link: '/shipping',
     },
   ];
   return (
     <div className="w-full p-4 space-y-8 flex flex-col justify-center items-center sm:flex-row sm:space-x-8">
       {cardInfo.map((card) => (
-        <Card />
+        <Card
+          image={card.image}
+          description={card.description}
+          link={card.link}
+        />
       ))}
     </div>
   );
