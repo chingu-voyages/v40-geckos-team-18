faa634a1b5b4f73c3765a837872f9411ea3a73d8
Cian Butler <67752829+ciandevelops@users.noreply.github.com>
2022-09-02 18:08:19 -0400

    id for key fixed

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -8,21 +8,25 @@ import shipping from '../assets/images/shipping.png';
 const Cards = () => {
   const cardInfo = [
     {
+      id: 0,
       image: plane,
       description: 'How much your flights cost to the environment?',
       link: '/travel',
     },
     {
+      id: 1,
       image: car,
       description: 'How much your car cost to the environment?',
       link: '/fuel',
     },
     {
+      id: 2,
       image: house,
       description: 'How much your house cost to the environment?',
       link: '/electricity',
     },
     {
+      id: 3,
       image: shipping,
       description: 'How much your online shopping cost to the environment?',
       link: '/shipping',
@@ -32,7 +36,7 @@ const Cards = () => {
     <div className="w-full p-4 space-y-8 flex flex-col justify-center items-center sm:flex-row sm:space-x-8">
       {cardInfo.map((card) => (
         <Card
-          key={card.image}
+          key={card.id}
           image={card.image}
           description={card.description}
           link={card.link}
