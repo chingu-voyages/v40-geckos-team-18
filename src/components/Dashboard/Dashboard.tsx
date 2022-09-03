0bfc8efdedfa2de93fdb18aa25c707900afa557e
ialej001 <ivan.alejandre@gmail.com>
2022-09-02 22:22:45 -0700

    added loading spinner

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { Spinner } from 'flowbite-react';
 import React from 'react';
 import { trpc } from '../../utils/trpc';
 import SummaryTile from './SummaryTile';
@@ -11,28 +12,31 @@ const Dashboard = () => {
   ];
   const { data, isLoading } = trpc.useQuery(['dashboard.summary']);
 
-  const totalEmissions = dummyData.reduce((previousValue, current) => {
+  const totalEmissions = data?.reduce((previousValue, current) => {
     return previousValue + current.emissions;
-  }, 0);
+  }, 0) ?? 0;
 
   console.log(data, totalEmissions);
 
   return (
     <div>
-      <div className="flex justify-center text-center">
+      <div className="flex flex-col justify-center text-center">
         <h2 className="text-2xl mb-5">Your total emissions to date</h2>
+        <h2 className='text-xl mb-5 font-bold'>{totalEmissions} g</h2>
       </div>
       <div className="flex justify-center text-center">
         <h4 className="text-2xl mb-5">
-          To this day, your total emissions break down as follows
+          To this date, your total emissions break down as follows
         </h4>
       </div>
-      <div className="flex flex-wrap justify-around content-around gap-5">
-        {dummyData.map((classification) => {
+      <div className="flex flex-wrap justify-around content-around gap-5 max-w-6xl min-w-xl mx-auto">
+        {isLoading && !data && <Spinner className='my-20' size='lg'/>}
+        {!isLoading && data && data.map((classification) => {
           return (
             <SummaryTile
               type={classification.type}
               emissionsValue={classification.emissions}
+              totalEmissions={totalEmissions}
               key={classification.type}
             />
           );
