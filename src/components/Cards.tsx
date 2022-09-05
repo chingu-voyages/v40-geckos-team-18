d6ffe88c06d98c4b4bb9d9924b8b1608f65258e8
Amanda Vieira <amanda.alexandre@outlook.com>
2022-09-05 15:20:11 -0300

    updating cards component

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import Card from './Card';
-import car from '../assets/images/car.png';
-import plane from '../assets/images/plane.png';
-import house from '../assets/images/house.png';
-import shipping from '../assets/images/shipping.png';
+import car from '../../public/car.png';
+import plane from '../../public/plane.png';
+import house from '../../public/house.png';
 
 const Cards = () => {
   const cardInfo = [
@@ -25,12 +24,6 @@ const Cards = () => {
       description: 'How much your house cost to the environment?',
       link: '/electricity',
     },
-    // {
-    //   id: 3,
-    //   image: shipping,
-    //   description: 'How much your online shopping cost to the environment?',
-    //   link: '/shipping',
-    // },
   ];
   return (
     <div className="w-full p-8 gap-8 flex flex-col justify-center items-center md:flex-row">
