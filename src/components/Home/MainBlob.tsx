3b4c56a3890e2f60640fbe9c3c9adc8fff4f6aa7
Amanda Vieira <amanda.alexandre@outlook.com>
2022-09-05 05:12:53 -0300

    style: image responsivity

--- src/components/Home/MainBlob.tsx
+++ src/components/Home/MainBlob.tsx
@@ -13,19 +13,22 @@ export default function MainBlob (props:BlobProps) {
  
     return (
         <div className="mainblob flex justify-center items-center">
-        <div className="grow basis-2/4">
+        <div className="grow basis-2/4 md:basis-full">
             <span className="text-5xl font-bold">{props.headline}</span>
             <p className="text-2xl">{props.text}</p>
         </div>
         
 
+        <div className="hidden md:inline shrink">
         <Image src={props.image}
             alt={props.headline} 
-            layout="fixed"
+
             width={363}
             height={500}
-            className="shrink"
+            className="main-blob-image"
         />
+        </div>
+
 
     </div>
     )
