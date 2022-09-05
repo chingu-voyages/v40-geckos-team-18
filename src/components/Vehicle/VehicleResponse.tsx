c850f104c3525cf939a76cbd509e323ed889500b
Amanda Alexandre <amanda.alexandre@outlook.com>
2022-09-05 03:01:09 -0300

    Development (#88)
    * Bug/navbar (#77)
    
    * z index issue fixed
    
    * Feature Create frontend UI for electricity calculator (#69)
    
    - added type for CountryCode in Electricty schema
        - Created Response and Form components for Electricity pages
        - Created json file to contain states and provinces for dropdown
        - Added states.json file containing states and provinces data
        - renamed watering plants image to remove spaces from filename
    
    * Feature Create frontend UI for Vehicle calculator (#74)
    
    - added more types to vehicle schema
    - added endpoints for vehicle makes and models to router
    - Created Questionaire, Response and Form components for Vehicle page
    - added react-select-search package for searchable dropdown component
    
    * Fixed hrefs and conditionally render links based on session
    
    Co-authored-by: Cian Butler <67752829+ciandevelops@users.noreply.github.com>
    Co-authored-by: Ticia <84279128+TiciaD@users.noreply.github.com>
    Co-authored-by: Ivan Alejandre <ivan.alejandre@gmail.com>

---
+++ src/components/Vehicle/VehicleResponse.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { VehicleResponse } from '../../schema/vehicle.schema';
+import { Button, Card } from 'flowbite-react';
+import Link from 'next/link';
+
+interface VehicleResponseProps {
+  data: VehicleResponse;
+}
+
+const VehicleResponse = ({ data }: VehicleResponseProps) => {
+  return (
+    <div className="mx-auto max-w-lg">
+      <Card>
+        <p>
+          This vehicle calculation for a{' '}
+          <u>
+            {data.vehicle_year} {data.vehicle_make} {data.vehicle_model}
+          </u>{' '}
+          on a trip covering an estimated{' '}
+          <u>
+            {data.distance_value}{' '}
+            {data.distance_unit == 'mi' ? 'miles' : 'kilometers'}
+          </u>{' '}
+          resulted in your carbon emissions contribution of:
+        </p>
+        <p className="flex justify-center mb-3">
+          <strong>{data.carbon_kg} kg</strong>
+        </p>
+        <hr></hr>
+        <p className=" text-center">
+          Thanks for using the vehicle carbon emissions calculator, please sign
+          up to save and track your carbon footprint!
+        </p>
+        <div className="flex justify-center">
+          <Button>
+            <Link href="register">Register</Link>
+          </Button>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default VehicleResponse;
--- src/server/router/vehicle.router.ts
+++ src/server/router/vehicle.router.ts
@@ -1,6 +1,8 @@
 import { createRouter } from './context';
-import { unregisteredVehicleRequestSchema } from '../../schema/vehicle.schema';
-import { carbonEstimateRequest } from '../../utils/apiHelpers';
+import z from 'zod';
+import { unregisteredVehicleRequestSchema, VehicleMakeDataResponse, VehicleMakeSearch, VehicleModelDataResponse, VehicleModelSearch } from '../../schema/vehicle.schema';
+import { carbonEstimateRequest, carbonVehicleMakeRequest, carbonVehicleModelsRequest } from '../../utils/apiHelpers';
+
 
 export const vehicleRouter = createRouter().mutation(
   'unregistered-request-vehicle',
@@ -16,4 +18,47 @@ export const vehicleRouter = createRouter().mutation(
       });
     },
   }
-);
+)
+.query(
+  'request-vehicle-make',
+  {
+    async resolve() {
+      const response = await fetch(
+        carbonVehicleMakeRequest()
+      );
+      let vehicleOptions: VehicleMakeSearch[] = []
+
+      return response.json().then((data) => {
+        const vehicles = data as VehicleMakeDataResponse[];
+        vehicleOptions = vehicles.map((vehicle: VehicleMakeDataResponse) => {
+          return {name: vehicle.data.attributes.name, value: vehicle.data.id}
+        })
+        return vehicleOptions;
+      }
+      )
+    }
+  }
+)
+.query(
+  'request-vehicle-model',
+  {
+    input: z.string(),
+    async resolve({input}) {
+      const response = await fetch(
+        // Query keeps firing on component load so added a fallback value to keep from getting invalid input errors
+        carbonVehicleModelsRequest(input ? input : '4c1e16e1-7967-4394-b3cb-15f4577dffa1')
+      );
+      let vehicleOptions: VehicleModelSearch[] = []
+
+      return response.json().then((data) => {
+        const vehicles = data as VehicleModelDataResponse[];
+        vehicleOptions = vehicles.map((vehicle: VehicleModelDataResponse) => {
+          const vehicleYearAndName = [vehicle.data.attributes.year, vehicle.data.attributes.name].join(' ');
+          return {name: vehicleYearAndName, value: vehicle.data.id}
+        })
+        return vehicleOptions;
+      }
+      )
+    }
+  }
+)
