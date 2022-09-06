ac185f6905e4a1b82d33456a45cca7ce408884bd
Amanda Vieira <amanda.alexandre@outlook.com>
2022-09-06 05:39:50 -0300

    style: responsive hero section

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -4,20 +4,26 @@ import Link from "next/link";
 import bikeWoman1 from "../../assets/images/bike-woman 1.png";
 
 const Hero = () => (
-    <div className="flex justify-center flex-col md:flex-row sm:flex-row p-10 gap-10">
-        <div>
-            <Image src={bikeWoman1} className="h-1 w-1" width={469} height={436} alt="Woman riding a bike" />
+    <div className="flex justify-center flex-col md:flex-row sm:flex-row p-10 gap-10 2xl:gap-20 xl:min-w-full">
+        <div className="xl:w-2/6">
+            <Image src={bikeWoman1} 
+                className="h-1 w-1" 
+                width={469} 
+                height={436} 
+                layout="responsive"
+                alt="Woman riding a bike" 
+            />
         </div>
-        <div className="flex flex-col justify-center gap-5" >
+        <div className="flex flex-col justify-center gap-5 xl:w-2/6" >
         
-                <p className="text-5xl font-bold dark:text-white">Estimate your carbon footprint</p>
+                <p className="text-5xl font-bold dark:text-white xl:text-8xl">Estimate your carbon footprint</p>
          
             <div className="flex">
                 <Link href="/register">
-                     <button type="button" className="focus:outline-none text-grey font-bold bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Start here</button>
+                     <button type="button" className="focus:outline-none text-grey font-bold bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 xl:text-xl">Start here</button>
                 </Link>
                 <Link href="/auth/login">
-                    <button type="button" className="object-none object-left-bottom focus:outline-none text-grey font-bold bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Login</button>
+                    <button type="button" className="object-none object-left-bottom focus:outline-none text-grey font-bold bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 xl:text-xl">Login</button>
                 </Link>
             </div>
         </div>  
