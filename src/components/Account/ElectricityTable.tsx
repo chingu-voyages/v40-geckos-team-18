d1b7d01ef4170bec9bfc9211dde78663ce8dd0ad
ialej001 <ivan.alejandre@gmail.com>
2022-09-07 12:18:08 -0700

    styling: table is responsive

--- src/components/Account/ElectricityTable.tsx
+++ src/components/Account/ElectricityTable.tsx
@@ -1,6 +1,5 @@
 import { Spinner, Table } from 'flowbite-react';
 import React from 'react';
-import { ElectricityData } from '../../schema/dashboard.schema';
 import { trpc } from '../../utils/trpc';
 
 const ElectricityTable = () => {
@@ -8,12 +7,13 @@ const ElectricityTable = () => {
     'dashboard.get-electicity-data',
   ]);
 
-//   const sortedElectricityData = () => {
-//     if (electricityData)
-//     return electricityData.sort((a: ElectricityData, b) => {
-//         return b.estimated_at - a.estimated_at;
-//     })
-//   }
+  const electricityDataDatesDesc = () => {
+    const dataToBeSorted = [...electricityData!];
+    if (electricityData)
+      return dataToBeSorted.sort(
+        (a, b) => b.estimated_at.valueOf() - a.estimated_at.valueOf()
+      );
+  };
 
   if (!electricityData) {
     return (
@@ -28,32 +28,43 @@ const ElectricityTable = () => {
       <Table hoverable={true}>
         <Table.Head>
           <Table.HeadCell>Date</Table.HeadCell>
-          <Table.HeadCell>Electricity Used</Table.HeadCell>
-          <Table.HeadCell>Emissions</Table.HeadCell>
+          <Table.HeadCell className="hidden sm:table-cell">
+            Electricity Used
+          </Table.HeadCell>
+          <Table.HeadCell className='hidden sm:table-cell'>Emissions</Table.HeadCell>
+          <Table.HeadCell className='sm:hidden'>
+            <div className='text-end'>
+              <p>Electricity Used</p>
+              <strong>Emissions</strong>
+            </div>
+          </Table.HeadCell>
         </Table.Head>
         <Table.Body className="divide-y">
           {electricityData.length !== 0 ? (
-            electricityData.map((entry) => {
+            electricityDataDatesDesc()!.map((entry) => {
               return (
                 <Table.Row
                   className="bg-white dark:border-gray-700 dark:bg-gray-800"
                   key={entry.id}
                 >
                   <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                    <p>
-                      {entry.estimated_at.toLocaleDateString()} at{' '}
-                      {entry.estimated_at.toLocaleTimeString()}
-                    </p>
+                    <p>{entry.estimated_at.toLocaleDateString()}</p>
+                    <p>{entry.estimated_at.toLocaleTimeString()}</p>
                   </Table.Cell>
                   {/** Todo: update this to use the users unit preference */}
-                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  <Table.Cell className="font-medium text-gray-900 dark:text-white hidden sm:table-cell">
                     <>
                       {entry.electricity_value} {entry.electricity_unit}
                     </>
                   </Table.Cell>
-                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  <Table.Cell className="font-medium text-gray-900 dark:text-white hidden sm:table-cell">
+                    <>{entry.carbon_g / 1000.0}kg</>
+                  </Table.Cell>
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 flex flex-col dark:text-white text-end sm:hidden">
                     <>
-                    {entry.carbon_g / 1000.0}kg</>
+                    {entry.electricity_value} {entry.electricity_unit}
+                    <strong>{entry.carbon_g / 1000.0}kg</strong>
+                    </>
                   </Table.Cell>
                 </Table.Row>
               );
@@ -63,6 +74,7 @@ const ElectricityTable = () => {
           )}
         </Table.Body>
       </Table>
+      {electricityData.length === 0 ? <div></div> : <div></div>}
     </div>
   );
 };
